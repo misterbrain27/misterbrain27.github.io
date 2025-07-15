c63ac7fd05c4657c31b960f1818e2766c0d3f88e
David Me <dabitbol1@me.com>
2025-07-15 17:27:11 +0200

    modify linked

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,10 +13,13 @@ export const Footer: React.FC = () => {
                       <a href="#" className="text-gray-400 hover:text-blue-400 transition-colors">
                           <Github size={24} />
                       </a>
-                      <a href="#" className="text-gray-400 hover:text-blue-400 transition-colors">
+                      <a href="https://www.linkedin.com/in/david-a-59a427ba/" className="text-gray-400 hover:text-blue-400 transition-colors"
+                         target="_blank">
                           <Linkedin size={24} />
                       </a>
-                      <a href="#" className="text-gray-400 hover:text-blue-400 transition-colors">
+                      <a href="https://www.linkedin.com/in/david-a-59a427ba/"
+                         target="_blank"
+                         className="text-gray-400 hover:text-blue-400 transition-colors">
                           <Mail size={24} />
                       </a>
                   </div>
